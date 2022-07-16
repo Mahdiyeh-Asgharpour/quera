df06e8452d0f4d928d3f50e8e709da7dfad3fbfb
Mahdiyeh <101658092+mahdiyeh-mazani@users.noreply.github.com>
2022-07-16 21:07:31 +0430

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+//package quera;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+       
+        String a[ ] = new String[5];
+        Scanner q=new Scanner(System.in);
+        for (int i=0;i<5;i++)
+            a[i]=q.nextLine();
+        for (int j=0;j<5;j++)
+
+            {
+                if (a[j].contains("HAFEZ") || a[j].contains("MOLANA"))
+                {
+                    System.out.print(j+1+" ");
+                }
+
+                 // else System.out.println("NOT FOUND!");
+            }
+
+    }
+}
