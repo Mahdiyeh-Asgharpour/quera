1e35365e9426dc31795121c1a6c2b7494f74ceff
Mahdiyeh <101658092+mahdiyeh-mazani@users.noreply.github.com>
2022-07-16 21:21:39 +0430

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,25 +1,21 @@
 //package quera;
 
 import java.util.Scanner;
-//KHOB BAGHER SARMA KHORDE
+
 public class Main {
 
     public static void main(String[] args) {
-       
-        String a[ ] = new String[5];
         Scanner q=new Scanner(System.in);
-        for (int i=0;i<5;i++)
-            a[i]=q.nextLine();
-        for (int j=0;j<5;j++)
-
-            {
-                if (a[j].contains("HAFEZ") || a[j].contains("MOLANA"))
-                {
-                    System.out.print(j+1+" ");
-                }
-
-                 // else System.out.println("NOT FOUND!");
-            }
-
+        String n=q.next();
+        for (int i=0;i<n.length();i++)
+        {
+            char e=n.charAt(i);
+            int w=Integer.parseInt(Character.toString(e));
+            System.out.print(w+": ");
+            for (int j=0;j<w;j++)
+                System.out.print(w);
+            System.out.println();
+        }
+	// write your code here
     }
 }
