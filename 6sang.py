61368d2e969aac42c021ca783c42c120b4b385bc
Mahdiyeh <www.mahdiyeh.mazani@gmail.com>
2024-01-28 17:51:12 +0330

    Add files via upload

---
+++ 6sang.py
@@ -0,0 +1,13 @@
+x=input()
+if x=="space":
+    print("blue")
+elif x=="mind":
+    print("yellow")
+elif x=="reality":
+    print("red")
+elif x=="power":
+    print("purple")
+elif x=="time":
+    print("green")
+else:
+    print("orange")
